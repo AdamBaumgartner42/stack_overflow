0fe0f1c999674b42e5e568af0ae83e6e5b58338d
Adam Baumgartner <adambaumgartner@gmail.com>
2022-04-03 21:40:39 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void addStack(int count, int a){
+void addStack(int count, int &a){
     count++; // index function count
     int b = 1; // create new variable on stack
     printf("%d: stack bottom: %p, current: %p \n", count, &a, &b);
